c9cb73bce199d7ce07c199893b35c2b205702522
Andrew Rogers <targetdisk1394@gmail.com>
2023-04-24 14:02:27 -0500

    Implement run-length encoding compression scheme
    Graphics are now compressed with a simple form of run-length encoding!
    This means that our "trident" executable can now shrink from ~400KiB
    down to ~35KiB!!  The savings are massive for the simple USB trident
    pixmap!  Graphics stored in RLE format are now represented in chunks
    consisting of two `uint32_t` values: RGBA pixel value and number of
    repetitions.
    
    Also in this commit are some changes to the configuration file JSON
    format.  All pixmaps are to be stored in the "gfx" array as dictionaries
    consisting of a name, dimensions [W,H], the source pixmap filename, and
    an enumerated type (see trident.h:6).

---
+++ trident.h
@@ -0,0 +1,36 @@
+#ifndef TRIDENT_H
+#define TRIDENT_H
+#include <stdint.h>
+#include <sys/ioctl.h>
+
+enum imgdata_type { RAW, RLE };
+
+typedef struct drawfren_s {
+  int fb_fd;
+  struct fb_fix_screeninfo finfo;
+  struct fb_var_screeninfo vinfo;
+  long screensize;
+  uint8_t *fbp;
+} drawfren_t;
+
+typedef struct pixmap_s {
+  int32_t width;
+  int32_t height;
+  size_t n_pixels;
+  char datatype;
+  uint32_t *data;
+} pixmap_t;
+
+#define GET_VINFO(fren) {                                  \
+  ioctl(fren->fb_fd, FBIOGET_VSCREENINFO, &fren->vinfo);   \
+}
+
+#define GET_FINFO(fren) {                                  \
+  ioctl(fren->fb_fd, FBIOGET_FSCREENINFO, &fren->finfo);   \
+}
+
+#define GET_INFOS(fren)  {  \
+  GET_VINFO(fren);          \
+  GET_FINFO(fren);          \
+}
+#endif /* TRIDENT_H */
